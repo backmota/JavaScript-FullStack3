7c5ee7b937f0dbe46c7b2a19d6be13ad3cc231d0
José Juan Mota <backmota@BackChai.local>
2022-11-03 20:04:52 -0600

    inner

---
+++ Modulo6-Clase12/js/events.js
@@ -0,0 +1,10 @@
+//https://developer.mozilla.org/es/docs/Web/API/EventTarget/addEventListener
+//Método addEventListener()
+
+//element.addEventListener(event, handler, options);
+
+/*
+event - Nombre del evento, cadena, por ejemplo "click".
+handler - Función de devolución de llamada que será llamada cuando ocurra el evento.
+options - Objeto de parámetro opcional con ajustes avanzados.
+*/
