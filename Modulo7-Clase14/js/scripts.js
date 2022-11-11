6f4dc87f0680070ddf569732794eb5969f60a664
José Juan Mota <backmota@BackChai.local>
2022-11-10 19:16:37 -0600

    Clase 14

--- Modulo7-Clase14/js/scripts.js
+++ Modulo7-Clase14/js/scripts.js
@@ -27,6 +27,10 @@ child.addEventListener("click", (event) => {
 });
 
 descendant.addEventListener("click", (event) => {
+  //Detener el ascenso
+  //Normalmente el evento aparecerá en el elemento window, llamando a todos los manejadores en su ruta. Pero cualquier gestor intermedio puede decidir que el evento es manejado completamente y se detiene el popping llamando al método stopPropagation().
+  //https://developer.mozilla.org/es/docs/Web/API/Event/stopPropagation
+  event.stopPropagation();
   console.log("Descendant click handler");
   console.log("event.target: ", event.target);
   console.log("event.currentTarget: ", event.currentTarget);
