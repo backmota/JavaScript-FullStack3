d748692f66a10cff7188a27f3930a3aa0a861287
José Juan Mota <backmota@BackChai.local>
2022-11-10 20:17:19 -0600

    scroll

--- Modulo7-Clase14/js/scroll.js
+++ Modulo7-Clase14/js/scroll.js
@@ -1,7 +1,25 @@
-const output = document.querySelector(".output");
-let scrollEventCounter = 0;
+/*
+https://cdnjs.com/libraries/lodash.js
+https://cdnjs.com/libraries
+https://lodash.com/docs/4.17.15
+https://lodash.com/docs/4.17.15#throttle
+*/
+const vanillaOutput = document.querySelector(".output.vanilla");
+const throttledOutput = document.querySelector(".output.throttled");
+const eventCounter = {
+  vanilla: 0,
+  throttled: 0
+};
 
 document.addEventListener("scroll", () => {
-	scrollEventCounter += 1;
-	output.textContent = scrollEventCounter;
+  eventCounter.vanilla += 1;
+  vanillaOutput.textContent = eventCounter.vanilla;
 });
+
+document.addEventListener(
+  "scroll",
+  _.throttle(() => {
+    eventCounter.throttled += 1;
+    throttledOutput.textContent = eventCounter.throttled;
+  }, 300)
+);
