feaf743652ee48a58e5b784d631010f2a9be6143
José Juan Mota <backmota@BackChai.local>
2022-11-10 19:00:34 -0600

    Clase 14

--- Modulo7-Clase13/js/scripts.js
+++ Modulo7-Clase13/js/scripts.js
@@ -18,7 +18,38 @@ removeListenerBtn.addEventListener("click", () => {
 	console.log("click event listener was removed from btn");
 });
 
+//Evento change
+/*
+Se produce después de que se haya modificado un elemento del formulario. Para los campos de texto o textarea, el evento no se producirá cada vez que se introduzca un carácter, sino cuando se pierda el enfoque, lo que no siempre es conveniente. Por ejemplo, mientras estás escribiendo algo en un campo de texto no hay ningún evento, pero tan pronto como se pierda el enfoque, se producirá el evento change. Para otros elementos, como select, casillas de verificación y botones de radio, el evento change se activa inmediatamente cuando se selecciona un valor.
+*/
+
 const select = document.querySelector(".pizza-select");
+const textOutput = document.querySelector(".text-output");
+const valueOutput = document.querySelector(".value-output");
+
+setOutput();
+
+select.addEventListener("change", setOutput);
+
+function setOutput() {
+	const selectedOptionValue = select.value;
+	const selectedOptionIndex = select.selectedIndex;
+	const selectedOptionText = select.options[selectedOptionIndex].text;
+
+	textOutput.textContent = selectedOptionText;
+  	valueOutput.textContent = selectedOptionValue;
+}
+
+//Evento input
+/*Ocurre sólo en los campos de texto y textarea, y se crea cada vez que se cambia el valor de un elemento, sin esperar a que se pierda el enfoque. En la práctica, input es el evento más importante para trabajar con los campos de texto de los formularios.*/
+
+const textInput = document.querySelector(".text-input");
+const output = document.querySelector(".output");
+
+textInput.addEventListener("input", (event) => {
+	console.log("Lllamada de la función");
+	output.textContent = event.currentTarget.value;
+});
 
 
 
