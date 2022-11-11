b9b5e18d8317a82d8fa1daed24849396eb55a779
José Juan Mota <backmota@BackChai.local>
2022-11-10 20:02:38 -0600

    scroll

--- Modulo7-Clase14/js/scroll.js
+++ Modulo7-Clase14/js/scroll.js
@@ -1,1 +1,7 @@
-scrolls.js
+const output = document.querySelector(".output");
+let scrollEventCounter = 0;
+
+document.addEventListener("scroll", () => {
+	scrollEventCounter += 1;
+	output.textContent = scrollEventCounter;
+});
