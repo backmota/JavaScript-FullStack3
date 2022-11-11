9aec3de2dcc55c21be75cae8fb449b3dad0ac59b
José Juan Mota <backmota@BackChai.local>
2022-11-10 19:18:06 -0600

    colors

--- Modulo7-Clase14/js/colors.js
+++ Modulo7-Clase14/js/colors.js
@@ -1,1 +1,4 @@
-colors.js
+const colorPalette = document.querySelector(".color-palette");
+const output = document.querySelector(".output");
+
+colorPalette.addEventListener("click", selectorColor);
